6aa18e35a34a00c552c89acc28bf61f18d8dfba3
AndresMatukInnovbo <andres.matuk@innovbo.com>
2026-06-17 21:44:28 -0500

    Remove unused screens and authentication service from the project.

--- lib/presentation/providers/product_provider.dart
+++
@@ -1,6 +0,0 @@
-export 'customer_catalog_provider.dart';
-export 'product_dependencies.dart';
-export 'product_detail_provider.dart';
-export 'product_mutation_provider.dart';
-export 'product_mutation_state.dart';
-export 'seller_products_provider.dart';
